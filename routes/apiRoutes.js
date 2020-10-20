db2d21630eb170171637ee2ae8c73be0e1e7daf7
gdickey273 <gdickey273@gmail.com>
2020-10-20 13:00:36 -0400

    started coding api routes. Just need to add route for get api/workouts/range request

---
+++ routes/apiRoutes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+  .sort({ date: -1 })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.update(
+    { _id: req.params.id},
+    { $push: { exercises: req.body } },
+    done)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
+
